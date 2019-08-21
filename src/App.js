208180c8ebd4c3cf2f28fc66b2c593a1bb966bfe
ShinySylveon04 <carlotta.basketball04@gmail.com>
2019-08-21 15:26:18 -0500

    Move theme to theme.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
-import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import { theme } from "./theme";
 import jwtDecode from "jwt-decode";
 import axios from "axios";
 
@@ -21,23 +21,6 @@ import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 import user from "./pages/user";
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: "#33c9dc",
-      main: "#00bcd4",
-      dark: "#008394",
-      contrastText: "#fff"
-    },
-    secondary: {
-      light: "#ff6333",
-      main: "#ff3d00",
-      dark: "#b22a00",
-      contrastText: "#fff"
-    }
-  }
-});
 
 axios.defaults.baseURL =
   "https://us-central1-socialape-38307.cloudfunctions.net/api";
